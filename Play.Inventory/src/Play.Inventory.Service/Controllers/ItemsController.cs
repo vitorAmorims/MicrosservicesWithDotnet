c0520060d0e9e727f85506ee3ce61456ca2d6495
vitorAmorims <vtamorims@gmail.com>
2022-08-20 13:59:47 -0300

    refactor initial with mediatR

--- Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
+++ Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit.Mediator;
 using Microsoft.AspNetCore.Mvc;
 using Play.Common;
 using Play.Inventory.Entities;
@@ -15,43 +16,61 @@ namespace Play.Inventory.Service.Controllers
     [Route("items")]
     public class ItemsController : ControllerBase
     {
-        private readonly IRepository<InventoryItem> inventoryItemsRepository;
-        private readonly IRepository<CatalogItem> catalogItemsRepository;
+        private readonly IMediator _mediator;
 
-        public ItemsController(IRepository<InventoryItem> inventoryItemsRepository, IRepository<CatalogItem> catalogItemsRepository)
+        public ItemsController(IMediator mediator)
         {
-            this.inventoryItemsRepository = inventoryItemsRepository;
-            this.catalogItemsRepository = catalogItemsRepository;
+            _mediator = mediator;
         }
+        // private readonly IRepository<InventoryItem> inventoryItemsRepository;
+        // private readonly IRepository<CatalogItem> catalogItemsRepository;
+
+        // public ItemsController(IRepository<InventoryItem> inventoryItemsRepository, IRepository<CatalogItem> catalogItemsRepository)
+        // {
+        //     this.inventoryItemsRepository = inventoryItemsRepository;
+        //     this.catalogItemsRepository = catalogItemsRepository;
+        // }
 
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
         {
+            // if (userId == Guid.Empty) return BadRequest();
+            // try
+            // {
+            //     var inventoryItemEntities = await inventoryItemsRepository.GetAllAsync(item => item.UserId == userId);
+            //     if (inventoryItemEntities.Count() == 0)
+            //         throw new DefaultException("UserId not exists");
+
+            //     var itemIds = inventoryItemEntities.Select(item => item.CatalogItemId);
+            //     var catalogItemEntities = await catalogItemsRepository.GetAllAsync(item => itemIds.Contains(item.Id));
+            //     if (catalogItemEntities.Count() == 0)
+            //         throw new DefaultException("CatalogItemId not exists");
+
+            //     var inventoryItemDtos = catalogItemEntities.Select(catalogItem =>
+            //     {
+            //         var inventoryItem = inventoryItemEntities.SingleOrDefault(i => i.CatalogItemId == catalogItem.Id);
+            //         return inventoryItem.AsDto(catalogItem.Name, catalogItem.Description);
+            //     });
+
+            //     return Ok(inventoryItemDtos);
+            // }
+            // catch (DefaultException ex)
+            // {
+            //     return NotFound(ex.Message);
+            // }
             if (userId == Guid.Empty) return BadRequest();
             try
             {
-                var inventoryItemEntities = await inventoryItemsRepository.GetAllAsync(item => item.UserId == userId);
-                if (inventoryItemEntities.Count() == 0)
-                    throw new DefaultException("UserId not exists");
-
-                var itemIds = inventoryItemEntities.Select(item => item.CatalogItemId);
-                var catalogItemEntities = await catalogItemsRepository.GetAllAsync(item => itemIds.Contains(item.Id));
-                if (catalogItemEntities.Count() == 0)
-                    throw new DefaultException("CatalogItemId not exists");
-
-                var inventoryItemDtos = catalogItemEntities.Select(catalogItem =>
-                {
-                    var inventoryItem = inventoryItemEntities.SingleOrDefault(i => i.CatalogItemId == catalogItem.Id);
-                    return inventoryItem.AsDto(catalogItem.Name, catalogItem.Description);
-                });
-
-                return Ok(inventoryItemDtos);
+                var inventoryItemDtos = await _mediator.Send(new GetAsyncRequest());
+                return Ok(inventoryItemDtos);    
             }
             catch (DefaultException ex)
             {
+                
                 return NotFound(ex.Message);
             }
+            
         }
 
         [HttpPost]
